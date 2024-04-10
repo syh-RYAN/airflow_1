43c6c2a339135afcd42d611d869167cf46a6e647
youngho <dudgh9477@naver.com>
2024-04-10 18:07:02 +0900

    template 오류

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -28,6 +28,6 @@ with DAG(
         print('ts:' +kwargs['ts'])
         print('data_interval_start:' + str(kwargs['data_interval_start']))
         print('data_interval_end' + str(kwargs['data_interval_end']))
-        print('task_instance:' + str(kwargs['t1']))
+        print('task_instance:' + str(kwargs['ti']))
         
     python_t1 >> python_function2() 
