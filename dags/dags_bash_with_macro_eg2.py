eaf2248b2bdfbed70f232a8c5c58c6f07c4fd9a8
youngho <dudgh9477@naver.com>
2024-11-03 19:34:08 +0900

    macros_수정

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id="dags_bash_with_macro_eg2",
-    schedule= "10 0 L * * 6#2",
+    schedule= "10 0 * * 6#2",
     start_date=pendulum.datetime(2023,3,1,tz="Asia/Seoul"),
     catchup=False
 ) as dag:
