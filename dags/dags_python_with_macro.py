d68f7cea08468f10c757f5bd04a65693a713e507
youngho <dudgh9477@naver.com>
2024-11-04 21:43:49 +0900

    241104_macro_2

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     
     @task(task_id = 'task_using_macors',
-          templates_dict={'start_date':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(months=-1,day=1))|ds}}',
+          templates_dict={'start_date':'{{(data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1,day=1))|ds}}',
                           'end_date' : '{{(data_interval_end.in_timezone("Asia/Seoul").replace(day=1)+macros.dateutil.relativedelta.relativedelta(days=-1)) |ds}}'
                           }
           )
