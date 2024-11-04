2fb39df3b63c679e460808340458b1b98b9ed99b
youngho <dudgh9477@naver.com>
2024-11-04 21:41:12 +0900

    241104_macro_2

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -28,7 +28,7 @@ with DAG(
         from dateutil.relativedelta import relativedelta
         
         data_interval_end = kwargs['data_interval_end']
-        prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(month=-1, day=1)
+        prev_month_day_first = data_interval_end.in_timezone('Asia/Seoul') + relativedelta(months=-1, day=1)
         prev_month_day_last = data_interval_end.in_timezone('Asia/Seoul').replace(day=1) + relativedelta(days=-1)
         print(prev_month_day_first.strftime('%Y-%m-%d'))
         print(prev_month_day_last.strftime('%Y-%m-%d'))
