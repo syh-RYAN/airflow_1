c41b3ef0fa4da0440052dce01118dc84c2035a8d
youngho <dudgh9477@naver.com>
2024-11-05 23:30:46 +0900

    xceom_241105

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -4,30 +4,30 @@ import datetime
 from airflow.decorators import task 
 
 with DAG(
-    dag_id='dags_python_with_xcome_eg1',
+    dag_id='dags_python_with_xcom_eg1',
     schedule='30 6 * * *',
     start_date=pendulum.datetime(2023,3,1,tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
-    @task(task_id = 'python_xcome_push_task1')
-    def xcome_push1(**kwargs):
+    @task(task_id = 'python_xcom_push_task1')
+    def xcom_push1(**kwargs):
         ti = kwargs['ti']
-        ti.xcome_push(key='result1',value='value_1')
-        ti.xcome_push(key='result2',value=[1,2,3])
+        ti.xcom_push(key='result1',value='value_1')
+        ti.xcom_push(key='result2',value=[1,2,3])
         
-    @task(task_id='python_xcome_push_task2')
-    def xcome_push2(**kwargs):
+    @task(task_id='python_xcom_push_task2')
+    def xcom_push2(**kwargs):
         ti = kwargs['ti']
-        ti.xcome_push(key='result1',value='value_2')
-        ti.xcome_push(key='result2',value=[1,2,3,4])
+        ti.xcom_push(key='result1',value='value_2')
+        ti.xcom_push(key='result2',value=[1,2,3,4])
         
-    @task(task_id = 'python_xcome_pull_task')
-    def xcome_pull(**kwargs):
+    @task(task_id = 'python_xcom_pull_task')
+    def xcom_pull(**kwargs):
         ti = kwargs['ti']
-        value1 = ti.xcome_pull(key='result1')
-        value2 = ti.xcome_pull(key='result2',task_ids='python_xcome_push_task1')
+        value1 = ti.xcom_pull(key='result1')
+        value2 = ti.xcom_pull(key='result2',task_ids='python_xcom_push_task1')
         print(value1)
         print(value2)
         
-    xcome_push1() >> xcome_push2() >> xcome_pull()
+    xcom_push1() >> xcom_push2() >> xcom_pull()
